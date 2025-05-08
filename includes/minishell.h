85d7bdc8da5e0ca889b66c34bcd2871d207d6ee2
MatthieuGillieron <gillieron.matthieu@ik.me>
2025-05-08 14:07:50 +0200

    feat(header): Add color

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: mg <mg@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/07 15:02:48 by mg                #+#    #+#             */
-/*   Updated: 2025/05/07 15:25:15 by mg               ###   ########.fr       */
+/*   Updated: 2025/05/08 14:07:26 by mg               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,9 @@
 # define MINISHELL_H
 
 
-//		** BIBLIOTHÈQUE **
+/*
+	Bibliotheque
+*/
 
 # include <stdio.h>
 # include <stdlib.h>
@@ -31,6 +33,18 @@
 # include <termios.h>
 
 
+/*
+	Colors
+*/
+
+# define RST		"\033[0m"		
+# define RED		"\033[1;31m"	
+# define GREEN		"\033[1;32m"	
+# define YELLOW		"\033[1;33m"	
+# define BLUE		"\033[1;34m"	
+# define MAGENTA	"\033[1;35m"	
+# define CYAN		"\033[1;36m"	
+# define WHITE		"\033[1;37m"
 
 
 

40ea0e7cfe3fe29f05034df96ddf18090dc30578
MatthieuGillieron <gillieron.matthieu@ik.me>
2025-05-26 14:18:34 +0200

    add(signal): Add enum for mode -> (inter, exec, heredoc)

--- includes/signals.h
+++ includes/signals.h
@@ -3,16 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   signals.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mtaramar <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*   By: mg <mg@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/08 15:12:30 by mtaramar          #+#    #+#             */
-/*   Updated: 2025/05/08 15:12:30 by mtaramar         ###   ########.fr       */
+/*   Updated: 2025/05/26 14:17:39 by mg               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SIGNALS_H
 # define SIGNALS_H
 
+typedef enum	e_signal_mode
+{
+	INTERACTIVE_MODE,
+	EXECUTION_MODE,
+	HEREDOC_MODE,
+}	t_signal_mode;
+
+//		*** PROTOTYPES ***
+
 void	init_signals(void);
+void	set_signal_mode(t_signal_mode mode);
+void	handle_signals_parent(void);
+void	handle_signals_child(void);
 
 #endif

9e7c1ebd4d8613abbec42d40de4d0ebd4b9f82cd
MatthieuGillieron <gillieron.matthieu@ik.me>
2025-06-10 17:13:47 +0200

    fxi(all): Fix perm. denied with /Users/mg/Desktop/dev/42/minishell

--- src/lexer/l_special.c
+++ src/lexer/l_special.c
@@ -2,10 +2,20 @@
 
 int	is_special_command(char *str, t_status *status)
 {
-	if (ft_strcmp(str, "$?") == 0)
+	// Traiter le cas où l'utilisateur tape directement une variable d'environnement
+	if (str[0] == '$' && str[1] && !is_whitespace(str[1]))
 	{
-		builtin_echo_status(status);
-		return (1);
+		char *var_name = extract_var_name(&str[1]);
+		if (var_name)
+		{
+			char *var_value = env_get(status->env, var_name);
+			if (var_value)
+				ft_putendl_fd(var_value, 1);
+			else
+				ft_putendl_fd("", 1);
+			free(var_name);
+			return (1);
+		}
 	}
 	return (0);
 }

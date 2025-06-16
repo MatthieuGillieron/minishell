699a1b19a074ab09ac17a6c2679cbdaa155f9331
MatthieuGillieron <gillieron.matthieu@ik.me>
2025-06-16 10:33:57 +0200

    norme(echo)

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -1,26 +1,34 @@
 #include "../includes/minishell.h"
 
-int	builtin_echo(char **args)
+static int	parse_echo_flags(char **args, int *i)
 {
-	int	i;
 	int	n_flag;
 	int	j;
 
 	n_flag = 0;
-	i = 1;
-	while (args[i] && args[i][0] == '-')
+	while (args[*i] && args[*i][0] == '-')
 	{
 		j = 1;
-		while (args[i][j] == 'n')
+		while (args[*i][j] == 'n')
 			j++;
-		if (args[i][j] == '\0' && j > 1)
+		if (args[*i][j] == '\0' && j > 1)
 		{
 			n_flag = 1;
-			i++;
+			(*i)++;
 		}
 		else
-			break;
+			break ;
 	}
+	return (n_flag);
+}
+
+int	builtin_echo(char **args)
+{
+	int	i;
+	int	n_flag;
+
+	i = 1;
+	n_flag = parse_echo_flags(args, &i);
 	while (args[i])
 	{
 		ft_putstr_fd(args[i], 1);
@@ -30,6 +38,5 @@ int	builtin_echo(char **args)
 	}
 	if (!n_flag)
 		ft_putchar_fd('\n', 1);
-	
 	return (0);
 }
